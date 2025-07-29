52e63d5d0eede8eb303d4cbd0fa0a9867bc8299e
Patrik Szabó <cablesalty@gmail.com>
2025-07-29 23:56:42 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+# Placeholder recognizer code, will be replaced
+with sr.Microphone() as source:
+    print("Talk")
+    audio_text = r.listen(source)
+    print("Time over, thanks")
+    
+    try:
+        # using google speech recognition
+        print("Text: "+r.recognize_google(audio_text))
+    except:
+         print("Sorry, I did not get that")
