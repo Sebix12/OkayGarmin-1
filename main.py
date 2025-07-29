eaa541a602ed5ad77f7ee76378ac8a159465a905
Patrik Szabó <cablesalty@gmail.com>
2025-07-30 00:35:07 +0200

    Rename incorrect file

--- main.py
+++ main.py
@@ -3,41 +3,40 @@ import simpleaudio as sa
 import keyboard
 import json
 
-manifest = {}
+settings = {}
 
 def clip():
     sa.WaveObject.from_wave_file("./sfx/garmin.wav").play()
-    keyboard.send(manifest["medal_keybind"])
+    keyboard.send(settings["medal_keybind"])
 
 def recognizer():
     r = sr.Recognizer()
 
     with sr.Microphone() as source:
+        print("Program is now listening")
         while True:
-            print("Running")
             audio_text = r.listen(source)
             
             try:
                 words = (str(r.recognize_google(audio_text, language="de-DE")).lower() + " " + str(r.recognize_google(audio_text, language="en-US")).lower())
                 print(words)
 
-                if any(item in words for item in manifest["pattern"]):
+                if any(item in words for item in settings["pattern"]):
                     clip()
                 else:
                     print("Did not detect anything")
-                    break
             except:
                 print("Failed to recognize voice")
 
 if __name__ == "__main__":
     # load application info from manifest
-    with open("manifest.json", "r") as f:
-        manifest = json.load(f)
+    with open("settings.json", "r") as f:
+        settings = json.load(f)
         f.close()
 
-    print(manifest)
+    print(settings)
 
-    print(f"OkayGarmin v{manifest["version"]}")
+    print(f"OkayGarmin v{settings["version"]}")
     print("By: cablesalty")
     print()
 
